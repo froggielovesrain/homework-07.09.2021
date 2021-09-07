c77b55ac742870ff8dd22263f1fedb0488600cac
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <87954254+froggielovesrain@users.noreply.github.com>
2021-09-07 18:31:43 +0200

    +addition/subtraction/multiplication/division

---
+++ 07.09.2021.wiecej dzialan.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+		float dzialanie;
+	cout<<"wpisz typ dzialania, ktore chcesz wykonac(dodawanie=1, odejmowanie=2, mnozenie=3, dzielenie=4) i wcisnij enter: \n";
+	cin>>dzialanie;
+	    
+	cout<<"wpisz liczbe pierwsza i wcisnij enter\n";
+	    
+	int liczba1;
+		cin>>liczba1;
+		
+    cout<<"wpisz liczbe druga i wcisnij enter\n";
+    int liczba2;
+		cin>>liczba2;
+		
+		cout<<"wynik twojego dzialania to:\n";
+
+	
+    	
+	if (dzialanie = 1);
+	cout<<(liczba1 + liczba2);
+	
+    if (dzialanie = 2);
+	cout<<(liczba1 - liczba2);
+	
+	if (dzialanie = 3);
+	cout<<(liczba1 * liczba2);
+	
+	if (dzialanie = 4);
+	cout<<(liczba1 / liczba2);
+		
+	return 0;
+}
+
+
