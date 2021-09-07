123b56e7ebdeb4511bbe6e687725309557480010
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <87954254+froggielovesrain@users.noreply.github.com>
2021-09-07 19:10:39 +0200

    addition/subtraction/multiplication/division

--- 07.09.2021.wiecej dzialan.cpp
+++ 07.09.2021.wiecej dzialan.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()
 {
-		float dzialanie;
-	cout<<"wpisz typ dzialania, ktore chcesz wykonac(dodawanie=1, odejmowanie=2, mnozenie=3, dzielenie=4) i wcisnij enter: \n";
+		int dzialanie;
+	cout<<"wpisz typ dzialania, ktore chcesz wykonac(dodawanie=1, odejmowanie=2, mnozenie=3, dzielenie=4) i wcisnij enter: \n"; 
 	cin>>dzialanie;
-	    
+	//nie wiem jak inaczej to zrobic, wyskakuje error za kazdym razem jak uzywam string zeby nie musiec wpisywac liczb tylko samo "dodawanie", "odejmowanie" itd	
+    
 	cout<<"wpisz liczbe pierwsza i wcisnij enter\n";
 	    
 	int liczba1;
@@ -17,22 +18,24 @@ int main()
 		cin>>liczba2;
 		
 		cout<<"wynik twojego dzialania to:\n";
-
+//nie wiem tez jak zrobic zeby nie byly te liczby w nowej linijce wiec robie wszystko w nowej
+	
 	
     	
-	if (dzialanie = 1);
-	cout<<(liczba1 + liczba2);
+	if(dzialanie == 1){
+	cout<<(liczba1+liczba2);
 	
-    if (dzialanie = 2);
-	cout<<(liczba1 - liczba2);
+    }else if(dzialanie == 2){
+	cout<<(liczba1-liczba2);
 	
-	if (dzialanie = 3);
-	cout<<(liczba1 * liczba2);
+	}else if(dzialanie == 3){
+	cout<<(liczba1*liczba2);
 	
-	if (dzialanie = 4);
-	cout<<(liczba1 / liczba2);
-		
+	}else{
+	(dzialanie == 4);
+	cout<<(liczba1/liczba2);
+   }
+
+
 	return 0;
 }
-
-
