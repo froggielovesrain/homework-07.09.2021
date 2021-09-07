bb1de3857394405fc38e1138b1d35a860cf8c20d
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <87954254+froggielovesrain@users.noreply.github.com>
2021-09-07 18:10:28 +0200

    multiplication/user input/two numbers

---
+++ 07.09.2021.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+		//ok
+	int liczba1;
+		cin>>liczba1;
+	
+		cout<<"*\n";
+    
+    int liczba2;
+		cin>>liczba2;
+		
+		cout<<"=";
+
+		
+		cout<<(liczba1 * liczba2);
+		
+		return 0;
+}
+
+
